a475db54a90b86f775ef7e7f8adfc79e24a7c1fb
Alpinoid <valentin.avramko@gmail.com>
2014-12-04 11:18:24 +0300

    Несколько изменений и реорганизаций.

--- Скрипты SQL/Измерения/13 Документы продаж.sql
+++ Скрипты SQL/Измерения/13 Документы продаж.sql
@@ -7,7 +7,7 @@ SET @StartDate = DATEADD(year, @Offset, '2014-01-01 00:00:00')
 SET @EndDate = DATEADD(year, @Offset, '2014-31-12 23:59:59')
 
 SELECT
-	CONVERT(varchar(32), DocSales._IDRRef, 2) AS ID
+	CONVERT(varchar(32), DocSales._IDRRef, 2) AS ID									-- ID документа
 	,DATEADD(year, -1*@Offset, CAST(DocSales._Date_Time AS date)) AS DocDate		-- Дата документа
 	,DocSales._Number AS DocNumber													-- Номер документа
 	,CONVERT(varchar(32), DocSales._Fld584RRef, 2) AS DocumentTypeID				-- ID типа документа
@@ -18,6 +18,7 @@ SELECT
 		ELSE DATEADD(year, -1*@Offset, CAST(DocSales._Fld1244 AS date))
 	END AS DocDeliveryDate															-- Дата доставки
 	,DATEADD(year, -1*@Offset, CAST(DocSales._Fld574 AS date)) AS DocPaymentDay		-- Дата оплаты
+	,CONVERT(int, DocSales._Fld1281) AS IsBonusDoc									-- Признак бонусного документа
 FROM dbo._Document81 AS DocSales
 WHERE DocSales._Posted = 0x01
 	AND DocSales._Date_Time BETWEEN @StartDate AND @EndDate
